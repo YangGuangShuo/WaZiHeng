8e29e39fe518197b91719c31dedc79d9eaa632b9
DESKTOP-2IPO030\22914 <229146930@qq.com>
2023-09-06 23:40:28 +0800

    规整资源

---
+++ Assets/Scripts/PlayerBalanceCtrl.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using Wazi;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class PlayerBalanceCtrl : MonoBehaviour
+{
+    private Animator mAnimator;
+
+    void Start()
+    {
+        mAnimator = gameObject.GetComponent<Animator>();
+        InputManager.Instance.OpenGyroscope();
+    }
+
+    void Update()
+    {
+        float _x = InputManager.Instance.Horizontal;
+        float _y = InputManager.Instance.Vertical;
+        Vector3 direction = Vector3.right * _x + Vector3.forward * _y;
+        float distance = Vector2.Distance(InputManager.Instance.Direction, Vector2.zero);
+        mAnimator.SetFloat("forward", distance);
+        direction = direction.normalized;
+        direction.y = 0;
+        mAnimator.SetFloat("right", _x);
+        Debug.Log(InputManager.Instance.Gyroscope.rotationRate);
+        if (direction.x != 0 || direction.y != 0)
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * 0.1f);
+    }
+}
