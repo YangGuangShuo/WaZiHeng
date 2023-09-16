c1a6c5833b2719d51938b67f21718590b8a3abde
AnYe <229146930@qq.com>
2023-09-16 19:18:20 +0800

    1

---
+++ Assets/Scripts/Managers/CharacterRotate.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CharacterRotate : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool _isDragging;
+    private Vector3 _prevPosition;
+    private float continuousDirection;
+    [SerializeField]
+    private Transform mPlayer;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (!_isDragging) return;
+        Vector2 mouseDelta = Input.mousePosition - _prevPosition;
+        _prevPosition = Input.mousePosition;
+
+        continuousDirection = -mouseDelta.x * 1;
+
+        if (continuousDirection.Equals(0))
+        {
+
+        }
+        else
+        {
+            InternalRotate(0, 100 * continuousDirection, 0);
+        }
+    }
+
+    private void InternalRotate(float x, float y, float z)
+    {
+        mPlayer.Rotate(x * Time.deltaTime, y * Time.deltaTime, z * Time.deltaTime);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerUp");
+        _isDragging = false;
+        continuousDirection = 0;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+        _prevPosition = Input.mousePosition;
+        _isDragging = true;
+    }
+}
